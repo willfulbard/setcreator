4b2f3399c8395f148a1eae8aac1a2637ce7d6a28
Will Wheeler <willfulbard@gmail.com>
2016-04-04 14:38:38 -0700

    Add missing comma

--- server/models/dances/danceModel.js
+++ server/models/dances/danceModel.js
@@ -1,8 +1,9 @@
 var mongoose = require('mongoose');
+// var Figure = require('figures/figureModel.js');
 
 var DanceSchema = new mongoose.Schema({
   name: String,
-  description: String
+  description: String,
   figures: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Figure'}]
 });
 
