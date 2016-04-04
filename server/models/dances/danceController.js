ef8ab5d04146ac30621a4dffd95020978dbbc35a
Will Wheeler <willfulbard@gmail.com>
2016-04-04 15:27:02 -0700

    Add newDance method to dances

---
+++ server/models/dances/danceController.js
@@ -0,0 +1,21 @@
+var Promise = require('bluebird');
+var Dance = require('./danceModel.js');
+
+var findAllDances = Promise.promisify(Dance.find.bind(Dance));
+var findDance = Promise.promisify(Dance.findOne.bind(Dance));
+
+module.exports = {
+  allDances: function (req, res, next) {
+    findAllDances({}).then(function (dances) {
+      res.json(dances);
+    }).catch(function(err) {
+      console.log('Error allDances in danceController.js: ', err.message);
+      next();
+    });
+  },
+
+  newDance: function (req, res, next) {
+    var danceName = req.body.danceName;
+    res.json(danceName);
+  }
+};
