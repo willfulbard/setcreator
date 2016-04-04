575e6bec4f3004511da6418dcf5e31321ccbb24c
Will Wheeler <willfulbard@gmail.com>
2016-04-04 14:40:07 -0700

    Add reference to local mongodb in server

--- server/server.js
+++ server/server.js
@@ -1,10 +1,10 @@
 var express = require('express');
-// var mongoose = require('mongoose');
+var mongoose = require('mongoose');
 
 var app = express();
 
-// connect to mongo database named "shortly"
-// mongoose.connect('mongodb://localhost/shortly');
+// connect to mongo database named "setcreator"
+mongoose.connect('mongodb://localhost/setcreator');
 //
 // // configure our server with all the middleware and routing
 require('./config/middleware.js')(app, express);
