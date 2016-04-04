67e389ff111a3782a1aa704ad3341661e78b8928
Will Wheeler <willfulbard@gmail.com>
2016-04-04 14:58:30 -0700

    Add api endpoints for getting all dances

--- server/config/routes.js
+++ server/config/routes.js
@@ -1,8 +1,8 @@
 // Include middleware for routes here
 
 module.exports = function (app, express) {
-  // Setup routes to api stuff here, like:
-  // app.get('/api/links/', linksController.allLinks);
-  // app.post('/api/links/', linksController.newLink);
+  // Setup routes to api stuff here:
+  app.get('/api/dances/', dancesController.allDances);
+  app.post('/api/dances/', dancesController.newLink);
 
 };
