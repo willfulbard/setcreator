da4026acfea4c6e50270a5cddb63b693f8cf44d6
Will Wheeler <willfulbard@gmail.com>
2016-04-04 15:29:20 -0700

    Remove reference to route that adds a dance

--- server/config/routes.js
+++ server/config/routes.js
@@ -4,6 +4,5 @@ var danceController = require('../models/dances/danceController.js');
 module.exports = function (app, express) {
   // Setup routes to api stuff here:
   app.get('/api/dances/', danceController.allDances);
-  app.post('/api/dances/', danceController.newDance);
 
 };
