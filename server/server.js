01a57b102a069654b09be3c258ba57bc9126150e
Will Wheeler <willfulbard@gmail.com>
2016-04-04 15:38:06 -0700

    Add connected notice

--- server/server.js
+++ server/server.js
@@ -5,13 +5,20 @@ var app = express();
 
 // connect to mongo database named "setcreator"
 mongoose.connect('mongodb://localhost/setcreator');
-//
-// // configure our server with all the middleware and routing
+var db = mongoose.connection;
+db.on('error', function (error) {
+  console.error('connection error:', error.message);
+});
+db.once('open', function() {
+  console.log('connected to db');
+});
+
+// configure our server with all the middleware and routing
 require('./config/middleware.js')(app, express);
 require('./config/routes.js')(app, express);
-//
-// // start listening to requests on port 8000
+
+// start listening to requests on port 8000
 app.listen(8000);
-//
-// // export our app for testing and flexibility, required by index.js
+
+// export our app for testing and flexibility, required by index.js
 module.exports = app;
