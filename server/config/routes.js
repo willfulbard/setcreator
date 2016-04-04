4cd2b649e70495ccc45abe1a3132d754f8f64dd6
Will Wheeler <willfulbard@gmail.com>
2016-04-04 12:06:25 -0700

    Create a basic server using node express

---
+++ server/config/routes.js
@@ -0,0 +1,8 @@
+// Include middleware for routes here
+
+module.exports = function (app, express) {
+  // Setup routes to api stuff here, like:
+  // app.get('/api/links/', linksController.allLinks);
+  // app.post('/api/links/', linksController.newLink);
+
+};
