30b0554c09fefd2ab24e8c2a60d84aefa9518931
Will Wheeler <willfulbard@gmail.com>
2016-04-04 16:36:51 -0700

    Add figureController to routes file

--- server/config/routes.js
+++ server/config/routes.js
@@ -1,8 +1,10 @@
 // Include middleware for routes here
 var danceController = require('../models/dances/danceController.js');
+var figureController = require('../models/figures/figureController.js');
 
 module.exports = function (app, express) {
   // Setup routes to api stuff here:
   app.get('/api/dances/', danceController.allDances);
+  app.get('/api/figures/', figureController.allFigures);
 
 };
