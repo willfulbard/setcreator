46131cb27cba27264d00a6b3c1d52daff8bfa829
Will Wheeler <willfulbard@gmail.com>
2016-04-04 21:18:49 -0700

    Add some state data that we'll need to keep track of

--- client/components/App.js
+++ client/components/App.js
@@ -2,7 +2,11 @@ class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      page: 'index'
+      page: 'index',
+      dance: {name: 'selected dance'},
+      figure: {name: 'selected figure'},
+      selectedTunes: [], //For tunes that have been selected so they are filtered
+      tunesForFigures: {} //Mapping of tune selections to figures
     };
   }
 
