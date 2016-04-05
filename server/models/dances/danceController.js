0d31b09779b42fc3fa509f4d6dc9b00b2ae8cd7c
Will Wheeler <willfulbard@gmail.com>
2016-04-05 16:07:12 -0700

    Add sorting of dances and figures

--- server/models/dances/danceController.js
+++ server/models/dances/danceController.js
@@ -7,7 +7,9 @@ var findDance = Promise.promisify(Dance.findOne.bind(Dance));
 module.exports = {
   allDances: function (req, res, next) {
     findAllDances({}).then(function (dances) {
-      res.json(dances);
+      res.json(dances.sort(function(a, b) {
+        return (a.name < b.name) ? -1 : (a.name > b.name) ? 1 : 0;
+      }));
     }).catch(function(err) {
       console.log('Error allDances in danceController.js: ', err.message);
       next();
--- server/models/figures/figureController.js
+++ server/models/figures/figureController.js
@@ -6,8 +6,10 @@ var findFigure = Promise.promisify(Figure.findOne.bind(Figure));
 
 module.exports = {
   allFigures: function (req, res, next) {
-    findAllFigures({}).then(function (dances) {
-      res.json(dances);
+    findAllFigures({}).then(function (figures) {
+      res.json(figures.sort(function(a, b) {
+        return (a.name < b.name) ? -1 : (a.name > b.name) ? 1 : 0;
+      }));
     }).catch(function(err) {
       console.log('Error allFigures in danceController.js: ', err.message);
       next();
