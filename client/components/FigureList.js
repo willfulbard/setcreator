c63bbdfd185542ef89390b016ae3fa84a36ba108
Will Wheeler <willfulbard@gmail.com>
2016-04-04 20:29:22 -0700

    Add TuneList and FigureList components

---
+++ client/components/FigureList.js
@@ -0,0 +1,12 @@
+class FigureList extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (<div>Figure List Here...</div>);
+  }
+}
+
+window.FigureList = FigureList;
+
---
+++ client/components/TuneList.js
@@ -0,0 +1,12 @@
+class TuneList extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (<div>Tune List Here...</div>);
+  }
+}
+
+window.TuneList = TuneList;
+
