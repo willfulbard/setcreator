1a0cdf3245bc6f0b1275ac4517e6fa7adaaa811d
Will Wheeler <willfulbard@gmail.com>
2016-04-05 14:56:28 -0700

    Add styling and make tune lists float to left or right

--- client/components/TuneList.js
+++ client/components/TuneList.js
@@ -12,6 +12,7 @@ class TuneList extends React.Component {
       console.log('tuneList: ', this.props.tunes.length);
       return (<div>
           <div className="tune-list">
+          <div>Tunes:</div>
           {this.props.tunes
             .filter(function(tune, index) {
               return this.props.selectedTunes.filter(function(selectedTune) {
@@ -24,6 +25,7 @@ class TuneList extends React.Component {
 
           </div>
           <div className="tunes-selected">
+          <div>Your Set:</div>
           {this.props.tunesForFigures[this.props.selectedFigure._id]
             .map(function(tune, index) { 
               return (<TuneSelected key={index} tuneSelected={tune} />); 
