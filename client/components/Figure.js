6654606e1392e607193fa8e51510dc08b04a5f0c
Will Wheeler <willfulbard@gmail.com>
2016-04-05 11:28:52 -0700

    Add new figure component

---
+++ client/components/Figure.js
@@ -0,0 +1,19 @@
+class Figure extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  selectFigure(e) {
+    console.log('figure selected', e);
+    e.data = {};
+    e.data.figure = this.props.figure;
+    e.data.event = 'selectFigure';
+  }
+
+  render() {
+    return (<div onClick={this.selectFigure.bind(this)}>{this.props.figure.name}</div>);
+  }
+}
+
+window.Figure = Figure;
+
--- client/components/FigureList.js
+++ client/components/FigureList.js
@@ -4,7 +4,13 @@ class FigureList extends React.Component {
   }
 
   render() {
-    return (<div>Figure List Here...</div>);
+    if (!this.props.selectedDance) {
+      return (<div>Please select a dance first</div>);
+    } else {
+      return (<div className="figure-list">
+        {this.props.figures.map(function(figure, index) { return (<Figure key={index} figure={figure} />); }.bind(this))}
+        </div>);
+    }
   }
 }
 
